ef8b15b2fadcce2c7bc794a1772c927486f62c23
Nastya <hurmuzz23@gmail.com>
2021-09-08 17:57:54 +0300

    day 07 - ex02 done

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -5,16 +5,76 @@
 #ifndef CPP06_ARRAY_HPP
 #define CPP06_ARRAY_HPP
 
-template <typename T>
+#include <iostream>
+
+template <class T>
 class Array
 {
 private:
-	T *_array;
+	T* _array;
+	unsigned int _len;
 
 public:
-	Array();
-	Array(unsigned int n);
-	~Array();
+
+	class IndexOutOfLimit : public std::exception
+	{
+	public:
+		virtual const char * what() const throw()
+		{
+			return ("Element is out of the limits");
+		}
+	};
+
+	Array()
+	{
+		_array = new T[0];
+		_len = 0;
+	}
+
+	Array(unsigned int n) : _len(n)
+	{
+		_array = new T[n];
+		for (unsigned int i = 0; i < n; i++)
+			_array[i] = 0;
+	}
+
+	Array(const Array<T> & src)
+	{
+		_len = src.size();
+		_array = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			_array[i] = src._array[i];
+	}
+
+	~Array()
+	{
+		delete [] _array;
+	}
+
+	Array<T> & operator=(const Array<T> & src)
+	{
+		if (this == &src)
+			return (*this);
+
+		delete [] _array;
+		_len = src.size();
+		_array = new T[_len];
+		for (unsigned int i = 0; i < _len; i++)
+			_array[i] = src._array[i];
+		return (*this);
+	}
+
+	T& operator[](unsigned int index)
+	{
+		if (index >= _len)
+			throw Array<T>::IndexOutOfLimit();
+		return (_array[index]);
+	}
+
+	unsigned int size(void) const
+	{
+		return (_len);
+	}
 };
 
 #endif //CPP06_ARRAY_HPP
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,6 +6,100 @@
 
 int main(void)
 {
+	{
+		std::cout << "---> int <---" << std::endl;
+		Array<int> first(5);
+
+		try
+		{
+			for (unsigned int i = 0; i < first.size(); i++)
+			{
+				first[i] = i;
+				std::cout << first[i] << " ";
+			}
+
+			std::cout << std::endl << "---------------------------"
+					  << std::endl;
+			std::cout << first[2] << std::endl;
+			std::cout << first[6] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "---------------------------" << std::endl;
+
+		{
+			Array<int> second;
+
+			second = first;
+			try
+			{
+				second[2] = 42;
+				std::cout << second[2] << std::endl;
+				std::cout << first[2] << std::endl;
+			}
+			catch (std::exception &e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+
+		try
+		{
+			std::cout << first[2] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+
+	std::cout << "---------------------------" << std::endl;
+
+	{
+		std::cout << "---> char <---" << std::endl;
+		Array<char> first(7);
+		char src[] = "Nastya";
+		unsigned int i;
+
+		try
+		{
+			for (i = 0; i < strlen(src); i++)
+			{
+				first[i] = src[i];
+				std::cout << first[i];
+			}
+			first[i] = src[i];
+
+			std::cout << std::endl << "---------------------------"
+					  << std::endl;
+			std::cout << first[2] << std::endl;
+			std::cout << first[9] << std::endl;
+		}
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+
+		std::cout << "---------------------------" << std::endl;
+
+		{
+			Array<char> second(first);
+
+			try
+			{
+				second[0] = 'n';
+				std::cout << second[0] << std::endl;
+				std::cout << first[0] << std::endl;
+			}
+			catch (std::exception &e)
+			{
+				std::cout << e.what() << std::endl;
+			}
+		}
+	}
 
 	return (0);
 }
